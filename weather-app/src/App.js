f531454d0c2da0b584937643da1a274e372d59e2
Phil Dugliss <phillipdugliss@gmail.com>
2019-10-24 11:59:55 -0500

    Initial Commit
    Initial Weather App commit

---
+++ weather-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Weather from './app_component/app.component';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Weather App</h1>
+    </div>
+  );
+}
+
+export default App;
