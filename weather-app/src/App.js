f2b988f22c346d92884acbde0291a80876f3ed51
Phil Dugliss <phillipdugliss@gmail.com>
2019-10-24 12:44:27 -0500

    API Call Made
    Open Weather API Call made successfully

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'weather-icons/css/weather-icons.css';
 import Weather from './app_component/app.component';
 
-function App() {
-  return (
-    <div className="App">
-     <Weather/>
-    </div>
-  );
+const API_KEY = "250d65d9d372b2222ad7aa1f9c5b68e5";
+//api.openweathermap.org/data/2.5/weather?q=London,uk
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {};
+    this.getWeather();
+  }
+
+  getWeather = async () => {
+    const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+    const res = await apiCall.json();
+    console.log(res);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Weather />
+      </div>
+    );
+  };
 }
 
 export default App;
--- weather-app/src/app_component/app.component.jsx
+++ weather-app/src/app_component/app.component.jsx
@@ -7,11 +7,26 @@ const Weather = () => {
                 <h1>London</h1>
             </div>
             <h5 className="py-4">
-                
+                <i className="wi wi-day-sunny display-1"></i>
             </h5>
+            <h1 className="py-2">
+                25&deg;
+            </h1>
+            {/**show max and min temp */}
+            {minMaxTemp(24,19)}
+            <h4 className="py-3">Slow Rain</h4>
+
         </div>
     );
+};
 
+function minMaxTemp(min, max){
+    return(
+        <h3>
+            <span className="span px-4">{min}&deg;</span>
+            <span className="span px-4">{max}&deg;</span>
+        </h3>
+    )
 }
 
 export default Weather; 
